3858da581c9e8ef0f80bcdbe9ee804fd2e49f3af
cconci <cconci.dev@gmail.com>
2016-03-06 11:18:41 +1100

    - initial commit of project

---
+++ app/src/main/java/com/cconci/gpsdistance/GPSMainActivity.java
@@ -0,0 +1,13 @@
+package com.cconci.gpsdistance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GPSMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gpsmain);
+    }
+}
